78ce6726d5f69bc7dbbfcb5d721d28921e83848a
Mitraansh Raaj Khanna <mitransh30@gmail.com>
2023-04-01 15:02:26 +0530

    Datatypes

---
+++ Primitives.java
@@ -0,0 +1,15 @@
+public class Primitives {
+    public static void main(String[] args){
+        int rollno=5;
+
+        // String is not primitive
+        String name="Kunal";
+
+        char letter='r';
+        float marks=98.67f;
+        double largeDecimalNumbers=45667775.4667;
+        long largeInteger = 340343076787674343L;
+        boolean check =false;
+        // System.out.println(rollno);   
+    }
+}
