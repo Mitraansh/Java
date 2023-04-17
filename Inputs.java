63a6407b71a47234f22868c4c36715c2dc85cb60
Mitraansh Raaj Khanna <mitransh30@gmail.com>
2023-04-17 22:16:46 +0530

    input line

--- Inputs.java
+++ Inputs.java
@@ -3,9 +3,16 @@ import java.util.Scanner;
 public class Inputs {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Please enter some Input ");
-        int rollno = input.nextInt();
-        System.out.println("Your number is "+rollno);
-        input.close();
+        // System.out.print("Please enter some Input ");
+        // int rollno = input.nextInt();
+        // System.out.println("Your number is "+rollno);
+        // input.close();
+
+        // To enter million we can use _ instead of , as _ will be ignored
+        //int a=234_000_000;
+        //System.out.println(a);
+
+        String name = input.nextLine();
+        System.out.println(name);
     }
 }
