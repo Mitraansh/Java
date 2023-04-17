163f9a76d44a7de8abe24df9325a08aab49cb3a8
Mitraansh Raaj Khanna <mitransh30@gmail.com>
2023-04-17 22:51:13 +0530

    basics

---
+++ basic.java
@@ -0,0 +1,18 @@
+public class basic {
+    public static void main(String[] args) {
+        //int a = 10;
+        //if(a==10)
+        // if(true){
+        //     System.out.println("Hello World");
+        // } 
+        // int count =1;
+        // while(count != 5){
+        //     System.out.println(count);
+        //     count++;
+        // }
+        int count;
+        for(count=1;count!=5;count++){
+            System.out.println(count);
+        }
+    }
+}
