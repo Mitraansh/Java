daf507b9ca674b3be67d1f0afb989bdded52a5e4
Mitraansh Raaj Khanna <mitransh30@gmail.com>
2023-04-17 22:41:28 +0530

    promotion in typecast

--- typecast.java
+++ typecast.java
@@ -2,8 +2,28 @@ import java.util.Scanner;
 
 public class typecast {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        float num = input.nextFloat();
+        // Scanner input = new Scanner(System.in);
+        // String num = input.nextLine();
+        // System.out.println(num);
+
+        // int num = (int)(67.56f);
+        // System.out.println(num);
+
+        // automatic type promotion in expression
+        // int a=257;
+        // byte b =(byte)(a);
+        // System.out.println(b); // 257%256
+
+        // byte a = 40;
+        // byte b =50;
+        // byte c=100;
+        // int d = (a*b)/c;
+        // System.out.println(d);
+
+        // byte b =50;
+        // b=b*5;
+
+        int num ='a';
         System.out.println(num);
     }
 }
